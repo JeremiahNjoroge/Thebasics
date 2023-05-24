6141261287258ddfdbd9a598eb8f7a46a59979c9
Jeremiah Njoroge <karurinjoroge00@gmail.com>
2023-05-24 15:04:59 +0300

    Added two new files

---
+++ hello.py
@@ -0,0 +1,5 @@
+def greeting():
+    user_input = input("Enter your name:")
+    name = user_input
+    print("Hello " +name+ " \nNice to meet you")
+greeting()
---
+++ rnd.py
@@ -0,0 +1,8 @@
+import random as rnd
+mylist = []
+while True:
+    
+    random = print(f"{rnd.randint(0,100)}")
+    mylist.append(random)
+    if len(mylist) >=2:
+        break
